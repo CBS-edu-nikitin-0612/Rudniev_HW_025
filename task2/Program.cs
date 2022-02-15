373294c0ddd8b0d2d0c3cb62f0cccb9e9cad322a
Vladyslav Rudniev <Rudniev.CSdev@gmail.com>
2022-02-15 13:25:46 +0200

    MyList<T>

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> intArray = new MyList<int>();
+            for (int i = 0; i < 10; i++)
+                intArray.Add(new Random().Next(0, 99));
+
+            foreach(int element in intArray)
+                Console.Write(element + "  ");
+        }
+    }
+}
