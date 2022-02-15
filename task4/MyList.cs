3a15c3335d7e02152ae921581c46580b0714ebbd
Vladyslav Rudniev <Rudniev.CSdev@gmail.com>
2022-02-15 14:07:00 +0200

    task4

---
+++ task4/MyList.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+
+namespace task4
+{
+    class MyList<T> : IEnumerable, IEnumerator
+    {
+        private T[] array;
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < array.Length)
+                    return array[index];
+                throw new IndexOutOfRangeException();
+            }
+        }
+        public int Count => array.Length;
+
+        public MyList()
+        {
+            array = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] temp = new T[array.Length + 1];
+            for (int i = 0; i < array.Length; i++)
+                temp[i] = array[i];
+            temp[array.Length] = item;
+            array = temp;
+        }
+        public void Remove(T item)
+        {
+            int n = -1, j = 0;
+            for (int i = 0; i < array.Length; i++)
+                if (array[i].GetHashCode() == item.GetHashCode())
+                    n = i;
+
+            if (n == -1)
+                return;
+
+            T[] temp = new T[array.Length - 1];
+            for (int i = 0; i < array.Length; i++)
+                if (i != n)
+                {
+                    temp[j] = array[i];
+                    j++;
+                }
+
+            array = temp;
+        }
+        public void Clear()
+        {
+            array = new T[0];
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return this as IEnumerator;
+        }
+
+        private int position = -1;
+        public object Current => array[position];
+        public bool MoveNext()
+        {
+            if (position < array.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            Reset();
+            return false;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+}
---
+++ task4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> intArray = new MyList<int>();
+            for (int i = 0; i < 10; i++)
+                intArray.Add(new Random().Next(0, 99));
+
+            int[] array = intArray.GetArray<int>();
+
+            foreach (int element in array)
+                Console.Write(element + "  ");
+        }
+    }
+
+    static class extendingClass
+    {
+        public static T[] GetArray<T>(this IEnumerable list)
+        {
+            MyList<T> myList = list as MyList<T>;
+            T[] array = new T[myList.Count];
+            for (int i = 0; i < myList.Count; i++)
+                array[i] = myList[i];
+            return array;
+        }
+    }
+}
